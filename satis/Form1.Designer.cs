17fe047654f5542dc61f8ae42f67ee05197947c9
Aytac <aytiquluzade@gmail.com>
2025-04-18 17:04:54 +0400

    Add project files.

---
+++ satis/Form1.Designer.cs
@@ -0,0 +1,429 @@
+﻿namespace satis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            returned = new Label();
+            total = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            label4 = new Label();
+            pay = new Label();
+            textBox2 = new TextBox();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btnx = new Button();
+            btn4 = new Button();
+            btndot = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btn0 = new Button();
+            btnentr = new Button();
+            label7 = new Label();
+            label8 = new Label();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Məhsul kodu:";
+            label1.Click += label1_Click;
+            // 
+            // returned
+            // 
+            returned.AutoSize = true;
+            returned.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            returned.Location = new Point(505, 128);
+            returned.Name = "returned";
+            returned.Size = new Size(174, 25);
+            returned.TabIndex = 1;
+            returned.Text = "Qaytarilan məbləg:";
+            // 
+            // total
+            // 
+            total.AutoSize = true;
+            total.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            total.Location = new Point(533, 30);
+            total.Name = "total";
+            total.Size = new Size(146, 25);
+            total.TabIndex = 2;
+            total.Text = "Ümümi məbləg:";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(185, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 60);
+            button1.TabIndex = 3;
+            button1.Text = "Kod siyahisi";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(185, 93);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 60);
+            button2.TabIndex = 4;
+            button2.Text = "Əlavə et";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(0, 74);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(160, 31);
+            textBox1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(20, 228);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(336, 279);
+            listBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(785, 30);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 25);
+            label4.TabIndex = 7;
+            label4.Text = "0.00 AZN";
+            // 
+            // pay
+            // 
+            pay.AutoSize = true;
+            pay.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            pay.Location = new Point(513, 80);
+            pay.Name = "pay";
+            pay.Size = new Size(162, 25);
+            pay.TabIndex = 8;
+            pay.Text = "Ödənilən məbləg:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(21, 36);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(286, 31);
+            textBox2.TabIndex = 9;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(9, 100);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(63, 41);
+            btn1.TabIndex = 11;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn2.Location = new Point(79, 100);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(63, 41);
+            btn2.TabIndex = 12;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn3.Location = new Point(169, 100);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(63, 41);
+            btn3.TabIndex = 13;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btnx
+            // 
+            btnx.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnx.Location = new Point(256, 100);
+            btnx.Name = "btnx";
+            btnx.Size = new Size(63, 41);
+            btnx.TabIndex = 14;
+            btnx.Text = "X";
+            btnx.UseVisualStyleBackColor = true;
+            btnx.Click += btnx_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn4.Location = new Point(9, 161);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(63, 41);
+            btn4.TabIndex = 15;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btndot
+            // 
+            btndot.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btndot.Location = new Point(256, 161);
+            btndot.Name = "btndot";
+            btndot.Size = new Size(63, 41);
+            btndot.TabIndex = 16;
+            btndot.Text = ".";
+            btndot.UseVisualStyleBackColor = true;
+            btndot.Click += btndot_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn6.Location = new Point(169, 161);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(63, 41);
+            btn6.TabIndex = 17;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn5.Location = new Point(79, 161);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(63, 41);
+            btn5.TabIndex = 18;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn9.Location = new Point(169, 230);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(63, 41);
+            btn9.TabIndex = 19;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn8.Location = new Point(79, 230);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(63, 41);
+            btn8.TabIndex = 20;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn7.Location = new Point(9, 230);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(63, 41);
+            btn7.TabIndex = 21;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn0.Location = new Point(256, 230);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(63, 41);
+            btn0.TabIndex = 22;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btnentr
+            // 
+            btnentr.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnentr.Location = new Point(9, 291);
+            btnentr.Name = "btnentr";
+            btnentr.Size = new Size(314, 41);
+            btnentr.TabIndex = 23;
+            btnentr.Text = "Hesabla";
+            btnentr.UseVisualStyleBackColor = true;
+            btnentr.Click += btnentr_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(785, 128);
+            label7.Name = "label7";
+            label7.Size = new Size(90, 25);
+            label7.TabIndex = 24;
+            label7.Text = "0.00 AZN";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(785, 83);
+            label8.Name = "label8";
+            label8.Size = new Size(90, 25);
+            label8.TabIndex = 25;
+            label8.Text = "0.00 AZN";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnentr);
+            groupBox1.Controls.Add(btn0);
+            groupBox1.Controls.Add(btn7);
+            groupBox1.Controls.Add(btn8);
+            groupBox1.Controls.Add(btn9);
+            groupBox1.Controls.Add(btn5);
+            groupBox1.Controls.Add(btn6);
+            groupBox1.Controls.Add(btndot);
+            groupBox1.Controls.Add(btn4);
+            groupBox1.Controls.Add(btnx);
+            groupBox1.Controls.Add(btn3);
+            groupBox1.Controls.Add(btn2);
+            groupBox1.Controls.Add(btn1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(533, 228);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(366, 343);
+            groupBox1.TabIndex = 26;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Məbləğ:";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkRed;
+            label2.Location = new Point(505, 200);
+            label2.Name = "label2";
+            label2.Size = new Size(393, 25);
+            label2.TabIndex = 27;
+            label2.Text = "Ödənilən  məbləğ ümumi məbləğdən kiçikdir";
+            label2.Visible = false;
+            label2.Click += label2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(344, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(113, 106);
+            pictureBox1.TabIndex = 28;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Bisque;
+            ClientSize = new Size(989, 616);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(groupBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(pay);
+            Controls.Add(label4);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(total);
+            Controls.Add(returned);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label returned;
+        private Label total;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Label label4;
+        private Label pay;
+        private TextBox textBox2;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btnx;
+        private Button btn4;
+        private Button btndot;
+        private Button btn6;
+        private Button btn5;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button btn0;
+        private Button btnentr;
+        private Label label7;
+        private Label label8;
+        private GroupBox groupBox1;
+        private Label label2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ satis/Form1.cs
@@ -0,0 +1,178 @@
+﻿using MessagingToolkit.QRCode.Codec;
+using System.Diagnostics.Eventing.Reader;
+
+namespace satis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        decimal totalp = 0;
+        string[] code = { "101", "102", "103" };
+        string[] name = { "Kurasan", "Coca-Cola", "Corek" };
+        decimal[] price = { 0.75M, 2.40M, 0.7M };
+        decimal totalprice = 0;
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"
+              Kod:101|Kurasan
+              Kod:102|Coca-Cola
+               Kod:103|Çörek
+
+                 ");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btndot_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void btnx_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void btnentr_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Mebleg daxil edin");
+            }
+            else if (totalprice == 0)
+            {
+                MessageBox.Show("Mehsul elave olunmayib");
+            }
+            else
+            {
+                decimal qiymet = decimal.Parse(textBox2.Text);
+
+                if (qiymet < totalprice)
+                {
+                    label2.Visible = true;
+                }
+                else
+                {
+                    label2.Visible = false;
+                    label8.Text = $" {qiymet.ToString()} AZN ";
+                    decimal qaytarilan = qiymet - totalprice;
+                    label7.Text = $" {qaytarilan.ToString()} AZN ";
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == code[0])
+            {
+                listBox1.Items.Add($"Kod:{code[0]} | Ad: {name[0]} | Qiymət: {price[0]} azn");
+                totalprice += price[0];
+
+            }
+            else if (textBox1.Text == code[1])
+            {
+                listBox1.Items.Add($"Kod:{code[1]} | Ad: {name[1]} | Qiymət: {price[1]} azn");
+                totalprice += price[1];
+            }
+            else if (textBox1.Text == code[2])
+            {
+                listBox1.Items.Add($"Kod:{code[2]} | Ad: {name[2]} | Qiymət: {price[2]} azn");
+                totalprice += price[2];
+            }
+            else
+            {
+                MessageBox.Show("Mehsul tapilmadi");
+            }
+            label4.Text = $" {totalprice.ToString()} AZN ";
+            decimal edv = totalprice / 100;
+            decimal per = edv * 18;
+
+            QRCodeEncoder enc = new QRCodeEncoder();
+            pictureBox1.Image = enc.Encode(per.ToString());// qr a cevir menimset
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
